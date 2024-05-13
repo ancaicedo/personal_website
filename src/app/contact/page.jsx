72c47c618ebd6f2f22445078d0cb9d6f27af7cd1
ancaicedo <77050076+ancaicedo@users.noreply.github.com>
2024-05-13 03:55:13 -0600

    trying to debug the emailjs server issue

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -19,6 +19,9 @@ const ContactPage = () => {
   }, []);
 
   const sendEmail = (e) => {
+    console.log("Service ID: ", process.env.NEXT_PUBLIC_SERVICE_ID);
+    console.log("Template ID: ", process.env.NEXT_PUBLIC_TEMPLATE_ID);
+    console.log("Public Key: ", process.env.NEXT_PUBLIC_PUBLIC_KEY);
     e.preventDefault();
     setError(false);
     setSuccess(false);
@@ -61,7 +64,8 @@ const ContactPage = () => {
               setSuccess(true);
               form.current.reset();
             },
-            () => {
+            (error) => {
+              console.log("EmailJS error: ", error);
               setError(true);
             }
         );
